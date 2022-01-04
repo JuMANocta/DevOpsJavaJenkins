05546f9201784554392490d2b20f908245b2dea6
JuMANocta <93329464+JuMANocta@users.noreply.github.com>
2022-01-04 14:40:02 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Bonjour, Jenkins token GitHub. Main Branch");
+        System.out.println("Bonjour, Jenkins token GitHub. new Main Branch");
     }
 }
